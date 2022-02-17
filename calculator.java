f2bb7254cd1611b722b15fab386dd983f54176e8
Aliasz1 <mzakwanas@protonmail.com>
2022-02-17 15:49:31 +0800

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -3,30 +3,27 @@ public class Calculator{
     //Methods will run the numbers respectively
     //method to add two numbers
     public static int Addition(int num1, int num2){
-        result = num1 + num2;
-        return result;
+        return (num1 + num2);
     }
 
     //method to subtract two numbers
     public static int Subtract(int num1, int num2){
-        result = num1 - num2;
-        return result;
+        return (num1 - num2);
     }
 
     //method to multiply two numbers
     public static int multiply(int num1, int num2){
-		result = num1*num2;
-		return result;
+		return (num1 * num2);
 	}
 
     //method to find the remainder of two numbers
     public static int remainder(int num1, int num2){
-		result = num1%num2;
-		return result;
+		return (num1 % num2);
 	}
    
     //method to find the greatest common denominator
 	public static int GCD(int num1, int num2){
+        int result;
         //checks if the second number is 0
 		if(num2 == 0){
 			result = num1;
@@ -42,6 +39,7 @@ public class Calculator{
    
     //method to find the lowest common multiple
 	public static int LCM(int num1, int num2){
+        int result, gcdresult;
 		gcdresult = GCD(num1, num2);
 		result = (num1*num2)/gcdresult;
 		return result;
