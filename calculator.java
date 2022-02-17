b68e32324ee76fe69dc8e564ffe7ae46051509b4
Aliasz1 <mzakwanas@protonmail.com>
2022-02-17 15:43:39 +0800

    Initial commit

---
+++ calculator.java
@@ -0,0 +1,56 @@
+public class Calculator{
+
+    //Methods will run the numbers respectively
+    //method to add two numbers
+    public static int Addition(int num1, int num2){
+        result = num1 + num2;
+        return result;
+    }
+
+    //method to subtract two numbers
+    public static int Subtract(int num1, int num2){
+        result = num1 - num2;
+        return result;
+    }
+
+    //method to multiply two numbers
+    public static int multiply(int num1, int num2){
+		result = num1*num2;
+		return result;
+	}
+
+    //method to find the remainder of two numbers
+    public static int remainder(int num1, int num2){
+		result = num1%num2;
+		return result;
+	}
+   
+    //method to find the greatest common denominator
+	public static int GCD(int num1, int num2){
+        //checks if the second number is 0
+		if(num2 == 0){
+			result = num1;
+		}
+		
+        //iterates until it reaches the common denominator
+		else{
+			result = GCD(num2, num1%num2);
+		}
+		
+		return result;
+		}
+   
+    //method to find the lowest common multiple
+	public static int LCM(int num1, int num2){
+		gcdresult = GCD(num1, num2);
+		result = (num1*num2)/gcdresult;
+		return result;
+	}
+
+    public static void main(String[] args){
+
+        int num1 = 1, num2 = 3;
+
+        System.out.println(Addition(num1, num2));
+    }
+}
